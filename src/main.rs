b52395b60bdbf4cb1071ca472bbb908cda8f0684
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2024-01-18 01:25:49 -0800

    expect->unwrap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-
 // an example to setup a tls server.
 // how to test:
 // curl https://localhost:12345 -k
-// or 
+// or
 // wget https://127.0.0.1:12345 --no-check-certificate -O - -q
 
 // You should see "Hello, World!" on your console.
 
 // to generate cert.pem and key.pem:
 // openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -nodes -subj '/CN=localhost'
-
 
 use bytes::Bytes;
 use http_body_util::Full;
@@ -23,7 +21,6 @@ use tokio::net::TcpListener;
 async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -48,16 +45,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         tokio::spawn(async move {
             // Accept the TLS connection.
-            let tls_stream = tls_acceptor.accept(socket).await.expect("accept error");
-
-            let tls_stream: hyper_tls::TlsStream<_> = tls_stream.into();  // not necessary
-
+            let tls_stream = tls_acceptor.accept(socket).await.unwrap();
+            let tls_stream: hyper_tls::TlsStream<_> = tls_stream.into(); // not necessary
             let io = TokioIo::new(tls_stream);
+            let service = service_fn(hello);
 
-            server
-                .serve_connection(io, service_fn(hello))
-                .await
-                .expect("serve error");
+            server.serve_connection(io, service).await.unwrap()
         });
     }
 }
